9841f68aec352bacd5e384fa9cf8c4c8f2991a44
Ales Rechtorik <foxxcz@gmail.com>
2019-08-28 01:21:22 +0200

    feat(transpiler): set messageformat params at path

--- projects/ngneat/transloco/src/lib/transpiler-strategies/messageformat.transpiler.ts
+++ projects/ngneat/transloco/src/lib/transpiler-strategies/messageformat.transpiler.ts
@@ -4,18 +4,6 @@ import { HashMap, Translation } from '../types';
 import * as MessageFormat from 'messageformat';
 import { isObject, set, getValue } from '../helpers';
 
-const iterate = (obj: Object, cb: Function) => {
-  Object.keys(obj).forEach(key => {
-    if (typeof obj[key] === 'object') {
-      iterate(obj[key], cb);
-    } else {
-      obj[key] = cb(obj[key]);
-    }
-  });
-
-  return obj;
-};
-
 export class MessageFormatTranspiler implements TranslocoTranspiler {
   defaultTranspiler: DefaultTranspiler = new DefaultTranspiler();
   //@ts-ignore
